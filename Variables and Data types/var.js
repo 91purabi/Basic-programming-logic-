0bdb3772c5afc73c6db843e6d4251510a834732f
Purabi <purbichoudhury06966@gmail.com>
2024-01-11 11:26:42 +0530

    var & data types

---
+++ Variables and Data types/var.js
@@ -0,0 +1,25 @@
+var age = 25;
+
+console.log("Age:", age);
+
+// age = 30; //Changing the value of the variable
+
+// console.log("Updated Age:", age); // Printing the updated value
+
+
+//DATA TYPES:
+
+// Number data type
+var age = 25;
+
+// Floating-point data type
+var height = 5.9;
+
+// String data type
+var name = "Purabi";
+
+// Boolean data type
+var isStudent = true;
+
+// Printing variables and their data types
+console.log("Age:", age, "Type:", typeof age);
